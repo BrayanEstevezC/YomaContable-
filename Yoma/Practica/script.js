dfdb12f158f0e871d2b9f07be0f948abf94c07e9
Brayan_Estevez <89996073+BrayanEstevezC@users.noreply.github.com>
2022-09-07 23:40:36 -0500

    Add files via upload

---
+++ Yoma/Practica/script.js
@@ -0,0 +1,57 @@
+console.log("%c                                             Practica","color: #1292EE; font-size: 2em");
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+console.log("Numeros pares del 0 al 100");
+let i, inpar=0, par=0;
+for(i = 0; i<100 ; i++)
+{
+    if(i % 2 !=0)
+    {
+        console.log(i);
+    }
+}
+console.log("numero pares");
+for(i =0; i<=100;i++)
+{
+    if(i%2 ==0)
+    {
+        console.log(i)
+    }    
+}
+console.log("numeros del 1 a 100");
+let j;
+for(j=0;j<101;j++)
+{
+    console.log(j);
+}
+console.log(" de 100 a 0");
+for(j =100; j>-1; j--)
+{
+    console.log(j);
+}
+console.log("multiplos de 2 y de 3");
+for(i =0; i<=100;i++)
+{
+    if(i%2 ==0 || i%3 ==0 )
+    {
+        console.log(i)
+    }    
+}*/
+let tablero 
+[
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+];
+let tableros 
+[
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+];
+tablero = [
+    [prompt("Ingrese X")],
+    [prompt("Ingrese y")],
+]
+console.log(tablero)
+
